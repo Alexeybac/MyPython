d35f194b74165a749e90ed0679db20626d2b9b22
Лёха <Alexbac@yandex.ru>
2023-03-30 12:19:41 +0300

    Fibonaccy

--- Fibonaccy/Fibonaccy.py
+++ Fibonaccy/Fibonaccy.py
@@ -3,11 +3,13 @@ a = int(input('Введем число А:  '))
 f1 = 0
 f2 = 1
 count = 2
-
+print(f1)
+print(f2)
 while a >= f2:
     if a == f2:
-        print(f'Порядковый номер числа А в ряде Фибоначчи: => {count}.')
+        print(f'Порядковый номер числа {a} в ряде Фибоначчи: => {count}.')
         break
+    print(f1 + f2)
     count += 1
     (f1, f2) = (f2, f2 + f1)
 else:
