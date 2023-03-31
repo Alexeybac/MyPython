0367ffa57eb28e2866c8ac02f7ca46210e4b133b
Лёха <Alexbac@yandex.ru>
2023-03-31 15:36:34 +0300

    3.2 Слайс

---
+++ Lesson_3/3.1.py
@@ -0,0 +1,7 @@
+txt = list()
+
+for i in range(8):
+    txt.append(input())
+
+print(txt)
+print(len(set(txt)))
---
+++ Lesson_3/3.2.py
@@ -0,0 +1,15 @@
+import os
+os.system('cls')
+n = int(input('Введите N '))
+txt = list()
+for i in range(n):
+    txt.append(i+1)
+print(txt)
+txt1 = txt
+k = int(input('Введите K '))
+# Через СЛАЙС (копии объекта)
+print(txt1[k%len(txt1):] + txt1[:k%len(txt1)])
+# Через цикл for
+for i in range(0, k): 
+    txt.insert(len(txt),txt.pop(0))
+print(txt)
