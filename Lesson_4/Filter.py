217e6e3dc622bf2dd0c2258db47cc9bf7d4def8a
Лёха <Alexbac@yandex.ru>
2023-04-01 22:19:27 +0300

    Filter

--- Lesson_4/Filter.py
+++ Lesson_4/Filter.py
@@ -1,7 +1,11 @@
 
-data = [12, 65, 28, 175, 95, 90]
+data = list(x for x in range(1,10,2))
+print(data)
 
 res = list(filter(lambda x: x % 10 == 5, data))
-
 print(res)
 
+list1 = list(enumerate(data))
+print(list1)
+list_1 = list(map(lambda x: (x[0]+1, x[1]), list1))
+print(list_1)
