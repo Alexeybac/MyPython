cfff0555a07f4946022fb9b8c7cf97e9a276f712
Лёха <Alexbac@yandex.ru>
2023-04-08 17:09:02 +0300

    40.py

--- Lesson_6/40.py
+++ Lesson_6/40.py
@@ -11,10 +11,14 @@
 # 1 2 3 4 5
 # 1 5 1 4 1
 # _ = input()
+from time import time
 nums = [int(i) for i in input('введите числа: ').split()]
+start = time()
 print(nums)
 count = 0
-for i in range(1, len(nums)-1):
-    if nums[i] == max(nums[i-1:i+2]):
-        count +=1
-print(count)
+# for i in range(1, len(nums)-1):
+#     if nums[i] == max(nums[i-1:i+2]):
+#         count +=1
+# print(count)
+print(len([i for i in range(1, len(nums)-1) if nums[i] == max(nums[i-1:i+2])]))
+print(time()-start)
