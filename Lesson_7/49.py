68320d2769aae5c541685479277084c2f69383b3
Лёха <Alexbac@yandex.ru>
2023-04-09 20:20:53 +0300

    49

--- Lesson_7/49.py
+++ Lesson_7/49.py
@@ -1,14 +1,7 @@
-
-orbits = [(1,3),(2.5,10),(7,2),(6,6),(4,3)]
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
 print(orbits)
 
 def find_far_orbit(x):
-    d = dict((3.14*i[0] * i[1], i) for i in x if i[0] != i[1])
-    return  d[max(d, key=d.get)]
-
-
-# def find_far_orbit(x):
-#     squears = list([3.14 * i[0] * i[1] for i in x if i[0] != i[1]])
-#     return orbits[squears.index(max(squears))]
-#
+    return max((3.14 * i[0] * i[1], i) for i in x if i[0] != i[1])[1]
 print('Результат => ', *find_far_orbit(orbits))
+
